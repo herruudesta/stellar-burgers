b7daff61038177fe7ac56d4b4d95a8a75f7af6f2
herruudesta <154591041+herruudesta@users.noreply.github.com>
2024-12-18 21:32:54 +0300

    cypress fix

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,14 +1,22 @@
 import * as orderFixture from '../fixtures/order.json';
 
+const BUN_SELECTOR = '[data-ingredient="bun"]:first-of-type';
+const MAIN_INGREDIENT_SELECTOR = '[data-ingredient="main"]:first-of-type';
+
+const MODAL_SELECTOR = '#modals';
+
+const ORDER_BUTTON_SELECTOR = '[data-order-button]';
+const MODAL_CLOSE_BUTTON_SELECTOR = `${MODAL_SELECTOR} button:first-of-type`;
+const MODAL_OVERLAY_SELECTOR = `${MODAL_SELECTOR} > div:nth-of-type(2)`;
+
 describe('E2E тест создания заказа', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
-
     cy.visit('/');
   });
 
   it('Протестировано добавление ингредиента из списка в конструктор.', () => {
-    cy.get('[data-ingredient="bun"]').should('have.length.at.least', 1);
+    cy.get(BUN_SELECTOR).should('have.length.at.least', 1);
     cy.get('[data-ingredient="main"],[data-ingredient="sauce"]').should(
       'have.length.at.least',
       1
@@ -18,37 +26,37 @@ describe('E2E тест создания заказа', () => {
   describe('Тестирование работы модальных окон.', () => {
     describe('Проверка открытия модальных окон', () => {
       it('Открытие модального окна ингредиента', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
-        cy.get('#modals').children().should('have.length', 2);
+        cy.get(BUN_SELECTOR).click();
+        cy.get(MODAL_SELECTOR).children().should('have.length', 2);
       });
 
       it('Открытие модального окна ингредиента при перезагрузке страницы', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get(BUN_SELECTOR).click();
         cy.reload(true);
-        cy.get('#modals').children().should('have.length', 2);
+        cy.get(MODAL_SELECTOR).children().should('have.length', 2);
       });
     });
 
     describe('Проверка успешности закрытия модальных окон и что конструктор пуст.', () => {
       it('Закрытие по клику на крестик', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
-        cy.get('#modals button:first-of-type').click();
+        cy.get(BUN_SELECTOR).click();
+        cy.get(MODAL_CLOSE_BUTTON_SELECTOR).click();
         cy.wait(500);
-        cy.get('#modals').children().should('have.length', 0);
+        cy.get(MODAL_SELECTOR).children().should('have.length', 0);
       });
 
       it('Закрытие по клику на оверлей', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
-        cy.get('#modals>div:nth-of-type(2)').click({ force: true });
+        cy.get(BUN_SELECTOR).click();
+        cy.get(MODAL_OVERLAY_SELECTOR).click({ force: true });
         cy.wait(500);
-        cy.get('#modals').children().should('have.length', 0);
+        cy.get(MODAL_SELECTOR).children().should('have.length', 0);
       });
 
       it('Закрытие по нажатию на Escape', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get(BUN_SELECTOR).click();
         cy.get('body').type('{esc}');
         cy.wait(500);
-        cy.get('#modals').children().should('have.length', 0);
+        cy.get(MODAL_SELECTOR).children().should('have.length', 0);
       });
     });
   });
@@ -65,21 +73,23 @@ describe('E2E тест создания заказа', () => {
       cy.visit('/');
     });
 
-    it('Оформление заказа авторизованным пользователем c последующим закрытием модалки', () => {
-      cy.get('[data-ingredient="bun"]:first-of-type button').click();
-      cy.get('[data-ingredient="main"]:first-of-type button').click();
-      cy.get('[data-order-button]').should('be.enabled');
+    it('Оформление заказа авторизованным пользователем c последующим закрытием модалки c перехватом запроса через intercept', () => {
+      cy.get(`${BUN_SELECTOR} button`).click();
+      cy.get(`${MAIN_INGREDIENT_SELECTOR} button`).click();
+      cy.get(ORDER_BUTTON_SELECTOR).should('be.enabled');
 
-      cy.get('[data-order-button]').click();
-      cy.get('#modals').children().should('have.length', 2);
+      cy.get(ORDER_BUTTON_SELECTOR).click();
+      cy.get(MODAL_SELECTOR).children().should('have.length', 2);
 
-      cy.get('#modals h2:first-of-type').should(
+      cy.get(`${MODAL_SELECTOR} h2:first-of-type`).should(
         'have.text',
         orderFixture.order.number
       );
 
-      cy.get('#modals button:first-of-type').click();
-      cy.get('#modals').children().should('have.length', 0);
+      cy.intercept('POST', 'api/orders').as('placeOrder');
+
+      cy.get(MODAL_CLOSE_BUTTON_SELECTOR).click();
+      cy.get(MODAL_SELECTOR).children().should('have.length', 0);
 
       cy.contains('Соберите бургер').children().should('have.length', 0);
     });
